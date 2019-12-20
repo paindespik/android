d8b0bc5aab93c5dad692663fc0209fd9355fe24d
paindespik <seandriard3@gmail.com>
2019-12-20 08:55:53 +0100

    new commit

--- RecyclerProjet/app/src/main/java/com/example/recyclerprojet/MainActivity.kt
+++ RecyclerProjet/app/src/main/java/com/example/recyclerprojet/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.recyclerprojet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.realm.Realm
+import io.realm.RealmObject
+import io.realm.annotations.RealmClass
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    @RealmClass
+    open class Player() : RealmObject() {
+        var name:String = ""
+        var score:Int = 0
     }
 }
